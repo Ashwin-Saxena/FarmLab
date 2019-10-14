353cb8ff4fa228bf854ed787d9faa98a5a7250c6
Ashwin Saxena <52866305+Ashwin-Saxena@users.noreply.github.com>
2019-10-14 23:40:19 +0530

    Night

--- app/src/main/java/com/android/farmstudio/Weather.java
+++ app/src/main/java/com/android/farmstudio/Weather.java
@@ -2,8 +2,20 @@ package com.android.farmstudio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.androdocs.httprequest.HttpRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class Weather extends AppCompatActivity {
 
@@ -16,10 +28,18 @@ TextView t1;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-        t1=findViewById(R.id.textView4);
-        t1.setText(CITY);
+        t1=findViewById(R.id.textView5);
         CITY =getIntent().getStringExtra("city");
+        t1.setText(CITY);
 
 
     }
+
+    public void wwe(View dssdd)
+    {
+
+
+    }
+
+
 }
